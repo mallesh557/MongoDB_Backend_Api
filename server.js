752b8effbe66281e5f19629db72a3c26615a587a
malli <malleshpotharaju9@gmail.com>
2026-03-03 09:19:44 +0530

    backend

---
+++ server.js
@@ -0,0 +1,33 @@
+const mongoose=require('mongoose'); 
+const express=require('express');
+const dotenv=require('dotenv');
+dotenv.config();
+const User=require('./user');
+const app=express();
+
+
+mongoose.connect(process.env.connectionString)
+.then(()=>{
+    console.log("connected to database");
+})
+.catch((err)=>{
+    console.log(err);
+})
+const port=process.env.PORT || 3000;
+app.use(express.json());
+app.listen(port,()=>{
+    console.log("server is running");
+})
+
+app.post("/users",async(req,res)=>{
+    const newuser=new User(req.body);
+    
+        const saveduser=await newuser.save();
+        res.json(saveduser);
+    
+})
+
+app.get("/users",async(req,res)=>{
+    const users=await User.find();
+    res.json(users);
+})
